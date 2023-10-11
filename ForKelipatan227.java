d4d9aef3c75812d9fe9935ea85086ecc13e7f18d
Satrio Dian Nugroho <satriodiannugroho@gmail.com>
2023-10-11 23:50:51 +0700

    Jobsheet 7 Percobaan 1

--- ForKelipatan227.java
+++ ForKelipatan227.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class ForKelipatan27 {
+public class ForKelipatan227 {
     public static void main(String[] args) {
 
         Scanner input27 = new Scanner(System.in);
 
-        int kelipatan, jumlah, counter;
+        int kelipatan,jumlah, counter;
 
         jumlah = 0;
         counter = 0;
+        float average = 0F;
 
         System.out.print("Masukkan Bilangan Kelipatan (1-9) : ");
         kelipatan = input27.nextInt();
@@ -17,10 +18,12 @@ public class ForKelipatan27 {
             if (a1 % kelipatan == 0) {
                 jumlah += a1;
                 counter++;
+                average = (float) jumlah/counter;
 
             }
             System.out.printf("Banyaknya Bilangan %d Dari 1 Sampai 50 Adalah %d\n",kelipatan,counter);
-            System.out.printf("Total Bilangan Kelipatan %d Dari 1 Sampai 50 Adalah %d\n\n",kelipatan,jumlah);
+            System.out.printf("Total Bilangan Kelipatan %d Dari 1 Sampai 50 Adalah %d\n",kelipatan,jumlah);
+            System.out.printf("Rata-Rata Dari Total Kelipatan %d Dari 1 Sampai 50 Adalah %f\n\n",kelipatan,average);
         }
 
     }
